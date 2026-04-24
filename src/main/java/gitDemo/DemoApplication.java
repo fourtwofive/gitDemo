651cdaddf5eb0f440bce5bbd7428db1a314bbd9c
zziony <wldnjs1123@gmail.com>
2026-04-24 14:32:44 +0900

    feat: C 개발중

--- src/main/java/gitDemo/DemoApplication.java
+++ src/main/java/gitDemo/DemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+
+		System.out.println("C 개발중");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
